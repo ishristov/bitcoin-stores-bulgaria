c801bea08d68ca729dc6b2928f32a8de962d9498
Ivaylo Hristov <ishristov@users.noreply.github.com>
2022-11-09 02:20:02 +0200

    fix filter issues with worpdress

--- src/components/TypesFilter.js
+++ src/components/TypesFilter.js
@@ -3,10 +3,16 @@ import { createUseStyles } from 'react-jss'
 import { useTranslation } from 'react-i18next'
 
 const useStyles = createUseStyles({
-  main: {
+  fieldset: {
     marginTop: 20,
-    borderColor: '#f6f6f6',
+    border: '2px groove #f6f6f6',
+    padding: '0.35em 0.65em',
+    lineHeight: '0.25em',
   },
+  legend: {
+    margin: 0,
+    padding: 2,
+  }
 })
 
 const TypesFilter = ({selectedTypes, allTypes, lang, handleTypesChange}) => {
@@ -14,7 +20,7 @@ const TypesFilter = ({selectedTypes, allTypes, lang, handleTypesChange}) => {
   const { t } = useTranslation()
 
   return (
-    <fieldset className={classes.main}>      
+    <fieldset className={classes.fieldset}>
       <legend>{t('merchantTypes')}</legend>
       {allTypes.map((type) => (
         <label key={type.idx} style={{display: 'block'}}>
